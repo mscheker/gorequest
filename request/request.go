015cac7fd9794643b66658703f4bbb686cec9a45
Maximo Scheker <maximo@healthnetwork.com>
2017-05-03 15:41:27 -0400

    Implemented doRequest method.

--- request/request.go
+++ request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -23,5 +24,25 @@ func Get(o *Options) {
 }
 
 func (r *Request) doRequest(m string, o *Options) (*http.Response, []byte, error) {
+	req, err := http.NewRequest(m, o.Url, nil)
 
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := r.client.Do(req)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		return resp, nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return resp, nil, err
+	}
+
+	return resp, body, nil
 }
