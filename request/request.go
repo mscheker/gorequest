c3b95a584992550f7b33587d71bfdd64944e3778
Maximo Scheker <maximo@healthnetwork.com>
2017-05-03 15:34:46 -0400

    Added Get and doRequest methods, and Options and Request structs.

--- request/request.go
+++ request/request.go
@@ -1,1 +1,27 @@
 package request
+
+import (
+	"net/http"
+	"time"
+)
+
+type Options struct {
+	Url string
+}
+
+type Request struct {
+	client  *http.Client
+	Timeout time.Duration
+}
+
+func (r *Request) Get(o *Options) {
+
+}
+
+func Get(o *Options) {
+
+}
+
+func (r *Request) doRequest(m string, o *Options) (*http.Response, []byte, error) {
+
+}
